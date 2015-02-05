f72c02570b06962996d7fc415e157e7bee8cab3c
Henning Henkel <henning.henkel@baloise.com>
2015-02-05 10:45:55 +0100

    Pulling in valid ifconfig.pp to escape merge hell

--- manifests/ifconfig.pp
+++ manifests/ifconfig.pp
@@ -11,8 +11,9 @@
 #
 # 
 define network_config::ifconfig (
-  $target,
+  $target = "/etc/sysconfig/network-scripts/ifcfg-${title}",
   $ipaddress=undef,
+  $interface_type=undef,
   $netmask=undef,
   $bootproto=undef,
   $defroute=undef,
@@ -32,112 +33,49 @@ define network_config::ifconfig (
   $gateway=undef,
   $ipv6_peerdns=undef,
   $ipv6_peerroutes=undef,
-  $interface_type=undef,
-  $zone=undef
+  $zone=undef,
+  $vlan=undef,
+  $device=undef,
+  $interface_name=$title,
+  $bonding_opts=undef,
+  $bonding_master=undef,
+  $slave=undef,
+  $master=undef,
 ) {
 
-  # The behaviour of ini_setting means that any value passed in as <<undef>> will
-  # be ignored and won't be managed, so we don't have to wrap everything in 
-  # conditionals.  It will however create a blank entry for anything defined as
-  # undef that isn't already in the sysconfig file, this is considered safe behaviour(?)
 
-
-  Ini_setting {
-    path              => $target,
-    ensure            => present,
-    section           => '',
-    key_val_separator => '=',
+  Network_config::Ifconfig::Setting {
+    target => $target,
   }
 
-  ini_setting {
-    "${title} netmask":
-      setting => 'NETMASK0',
-      value => $netmask;
-
-    "${title} bootproto":
-      setting => 'BOOTPROTO',
-      value => $bootproto;
-
-    "${title} defroute":
-      setting => 'DEFROUTE',
-      value => $defroute;
-
-    "${title} ipv4_failure_fatal":
-      setting => 'IPV4_FAILURE_FATAL',
-      value => $ipv4_failure_fatal;
-
-    "${title} ipv6init":
-      setting => 'IPV6INIT',
-      value => $ipv6init;
-
-    "${title} ipv6_autoconf":
-      setting => 'IPV6_AUTOCONF',
-      value => $ipv6_autoconf;
-
-    "${title} ipv6_defroute":
-      setting => 'IPV6_DEFROUTE',
-      value => $ipv6_defroute;
-
-    "${title} ipv6_failure_fatal":
-      setting => 'IPV6_FAILURE_FATAL',
-      value => $ipv6_failure_fatal;
-
-    "${title} uuid":
-      setting => 'UUID',
-      value => $uuid;
-
-    "${title} onboot":
-      setting => 'ONBOOT',
-      value => $onboot;
-
-    "${title} dns1":
-      setting => 'DNS1',
-      value => $dns1;
-
-    "${title} dns2":
-      setting => 'DNS2',
-      value => $dns2;
-
-    "${title} domain":
-      setting => 'DOMAIN',
-      value => $domain;
-
-    "${title} hwaddr":
-      setting => 'HWADDR',
-      value => $hwaddr;
-
-    "${title} ipaddr":
-      setting => 'IPADDR0',
-      value => $ipaddr;
-
-    "${title} prefix":
-      setting => 'PREFIX0',
-      value => $prefix;
-
-    "${title} gateway":
-      setting => 'GATEWAY0',
-      value => $gateway;
-
-    "${title} ipv6_peerdns":
-      setting => 'IPV6_PEERDNS',
-      value => $ipv6_peerdns;
-
-    "${title} ipv6_peerroutes":
-      setting => 'IPV6_PEERROUTES',
-      value => $ipv6_peerroutes;
-
-    "${title} zone":
-      setting => 'ZONE',
-      value => $zone;
-
-    "${title} int_name":
-      setting => 'NAME',
-      value => $int_name;
-
-    "${title} type":
-      setting => 'TYPE',
-      value => $interface_type;
-    }
-
+  network_config::ifconfig::setting {
+    "${title}:netmask":  value => $netmask;
+    "${title}:bootproto":  value => $bootproto;
+    "${title}:defroute":  value => $defroute;
+    "${title}:ipv4_failure_fatal": value => $ipv4_failure_fatal;
+    "${title}:ipv6init": value => $ipv6init;
+    "${title}:ipv6_autoconf": value => $ipv6_autoconf;
+    "${title}:ipv6_defroute": value => $ipv6_defroute;
+    "${title}:ipv6_failure_fatal": value => $ipv6_failure_fatal;
+    "${title}:uuid": value => $uuid;
+    "${title}:onboot": value => $onboot;
+    "${title}:dns1": value => $dns1;
+    "${title}:dns2": value => $dns2;
+    "${title}:domain": value => $domain;
+    "${title}:hwaddr": value => $hwaddr;
+    "${title}:ipaddr": value => $ipaddr;
+    "${title}:prefix": value => $prefix;
+    "${title}:gateway": value => $gateway;
+    "${title}:ipv6_peerdns": value => $ipv6_peerdns;
+    "${title}:ipv6_peerroutes": value => $ipv6_peerroutes;
+    "${title}:zone": value => $zone;
+    "${title}:interface_type": value => $interface_type, setting => "TYPE";
+    "${title}:device": value => $device;
+    "${title}:interface_name": value => $interface_name, setting => "NAME";
+    "${title}:bonding_opts": value => $bonding_opts;
+    "${title}:bonding_master": value => $bonding_master;
+    "${title}:master": value => $master;
+    "${title}:slave": value => $slave;
+  }
 }
-  
+
